630a1600e8cf7402fa08bd28321ea7766459e719
Popov <flash.royal473@gmail.com>
2020-12-11 00:02:02 +0300

    admin.py look

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Contract, Task
 
-admin.site.register(Contract)
-admin.site.register(Task)
+# Define the admin class
+@admin.register(Contract)
+class ContractAdmin (admin.ModelAdmin):
+    list_display = ('zakupkiId','name','dateStart','dateEnd')
+
+@admin.register(Task)
+class TaskAdmin (admin.ModelAdmin):
+    list_display = ('name','description','contract','datetimeStart','datetimeEnd','status')
+# admin.site.register(Contract, ContractAdmin)
+# admin.site.register(Contract)
+#admin.site.register(Task)
 # Register your models here.
