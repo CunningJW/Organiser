575059651ada2bb3ca78f7e0f5de71c60d9bab59
CunningJW <cunningjewmm@gmail.com>
2020-12-10 22:02:09 +0300

    added basic models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Conctract(models.Model):
+    name = models.CharField(max_length=100, help_text="Enter a city name for a contract")
+    zakupkiId = models.CharField(max_length=50, help_text="Enter an id from zakupki.gov.ru")
+
+
+    def __str__(self):
+        return self.title
+
+
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter a task name")
+    contract = models.ForeignKey('Contract', on_delete=models.SET_NULL, null=True)
+
+
+    def __str__(self):
+        return self.title
